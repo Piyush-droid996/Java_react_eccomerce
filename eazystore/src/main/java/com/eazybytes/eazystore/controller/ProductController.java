3485211ec7f875d6caffa6218813e8ece4a2b9b7
Piyush saxena <72535689+Piyush-droid996@users.noreply.github.com>
2026-07-01 17:30:29 +0530

    backend changes

--- eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.eazybytes.eazystore.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@CrossOrigin(origins="http://localhost:5173")
 public class ProductController {
 
     private final IProductService iProductService;
